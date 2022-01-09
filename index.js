a2ef3b00cd8a5b24dbcd5ceed3166889bfe0a8be
alinelina <alinelina2013@gmail.com>
2022-01-09 18:12:27 +0200

    added functions

---
+++ index.js
@@ -0,0 +1,23 @@
+function calcAddition() {
+    let x = Number(prompt('Введите первое число:'));
+    let y = Number(prompt('Введите второе число:'));
+    return (x + y);
+}
+
+function calcSubtraction() {
+    let x = Number(prompt('Введите первое число:'));
+    let y = Number(prompt('Введите второе число:'));
+    return (x - y);
+}
+
+function calcMultiplication() {
+    let x = Number(prompt('Введите первое число:'));
+    let y = Number(prompt('Введите второе число:'));
+    return (x * y);    
+}
+
+function calcDivision() {
+    let x = Number(prompt('Введите первое число:'));
+    let y = Number(prompt('Введите второе число:'));
+    return (x / y);
+}
